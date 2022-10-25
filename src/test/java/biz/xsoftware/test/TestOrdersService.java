e30a78f2ce66bd93ffc92e7a2934ea9e039697eb
Dean Hiller <dean.hiller@tray.com>
2022-10-25 13:01:00 +0530

    add comments and such for the developers to get started

--- src/test/java/biz/xsoftware/test/TestOrdersService.java
+++ src/test/java/biz/xsoftware/test/TestOrdersService.java
@@ -1,12 +1,20 @@
 package biz.xsoftware.test;
 
+import biz.xsoftware.caching.api.CachedResponse;
+import biz.xsoftware.caching.api.CachingRequest;
+import biz.xsoftware.orders.api.OrderRequest;
+import biz.xsoftware.orders.api.OrderResponse;
 import biz.xsoftware.orders.api.OrdersService;
 import biz.xsoftware.test.FeatureTest;
+import org.junit.Assert;
 import org.junit.Test;
 
-public class TestOrdersService extends FeatureTest {
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
-	protected OrdersService userSvc;
+public class TestOrdersService extends FeatureTest {
 
 	@Override
 	protected void tearDown() throws Exception {
@@ -14,7 +22,19 @@ public class TestOrdersService extends FeatureTest {
 	}
 
 	@Test
-	public void testFetchOrder() {
+	public void testFetchOrder() throws ExecutionException, InterruptedException, TimeoutException {
+
+		//Create a mock response for mockCacheSvc variable here
+		//populate mock with a response for later here
+
+		//create a OrderRequest here and invoke ordersSvc
+		//call the service here ordersSvc.readOrder(request).get(5, TimeUnit.SECONDS);
+		//make sure you assigned the response to a variable
+
+		//validate the OrderResponse returned here
+
+		// go to the mockCacheSvc and grab the request it received here
+		//validate the correct request was sent to mockCacheSvc
 
 	}
 
